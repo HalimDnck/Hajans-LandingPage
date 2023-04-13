ceabc4fc76fc1bfa60685b5e07cff1962a92f858
Halim Dinckalmis <halimdinc0@gmail.com>
2023-04-13 04:51:13 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+//Size selection
+/*
 $("body").on("click", "label", function(e) {
    var getValue = $(this).attr("for");
    var goToParent = $(this).parents(".select-size");
    var getInputRadio = goToParent.find("input[id = " + getValue + "]");
    console.log(getInputRadio.attr("id"));  
- });
+ });*/
  
+//Image Slide
+var imageBtn1 = document.getElementById("s1")
+var imageBtn2 = document.getElementById("s2")
+var imageBtn3 = document.getElementById("s3")
+
+imageBtn1.addEventListener("click", function(){
+   document.getElementById("image").src = "images/1_silver.png"
+   imageBtn1.style.border = "2px solid black"
+   imageBtn2.style.border = "none"
+   imageBtn3.style.border = "none"
+})
+imageBtn2.addEventListener("click", function(){
+   document.getElementById("image").src = "images/2_silver.png"
+   imageBtn2.style.border = "2px solid black"
+   imageBtn1.style.border = "none"
+   imageBtn3.style.border = "none"
+})
+imageBtn3.addEventListener("click", function(){
+   document.getElementById("image").src = "images/3_silver.png"
+   imageBtn3.style.border = "2px solid black"
+   imageBtn2.style.border = "none"
+   imageBtn1.style.border = "none"
+})
+
+//Color options
